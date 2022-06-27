a1579373160163463f41dd5859a846c40c42f89a
gyubin <gbcompany27@gmail.com>
2022-06-28 00:04:08 +0900

    add page back function to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom';
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from 'react';
+import { createBrowserHistory } from "history";
 
 import HomePage from './components/views/HomePage/HomePage';
 import StockPage from './components/views/StockPage/StockPage';
@@ -15,17 +16,45 @@ import IndustryPage from './components/views/IndustryPage/IndustryPage';
 import PortfolioPage from './components/views/PortfolioPage/PortfolioPage';
 import SimulationPage from './components/views/SimulationPage/SimulationPage';
 
-import {getCurrentPrice} from './_reducers/stock';
 import { changePage } from './_reducers/pager';
 
 
 function App() {
+  const currentPath = window.location.pathname;
   const dispatch = useDispatch();
   // useEffect(()=> {
   //   dispatch(getCurrentPrice('aapl'));
   // },[]);
+  const history = createBrowserHistory();
+  useEffect(() => {
+    const listenBackEvent = () => {
+      if (currentPath === "/") {
+        dispatch(changePage('대쉬보드 홈'));
+      } else if (currentPath === "/Stock") {
+        dispatch(changePage('주식 데이터'));
+      } else if (currentPath === "/Economy") {
+        dispatch(changePage('경제 데이터'));
+      } else if (currentPath === "/Industry") {
+        dispatch(changePage('산업 데이터'));
+      } else if (currentPath === "/Portfolio") {
+        dispatch(changePage('포트폴리오'));
+      } else if (currentPath === "/Simulation") {
+        dispatch(changePage('시뮬레이션'));
+      }
+    };
 
-  const currentPath = window.location.pathname;
+    const unlistenHistoryEvent = history.listen(({ action }) => {
+      if (action === "POP") {
+        listenBackEvent();
+      }
+    });
+
+    return unlistenHistoryEvent;
+  }, [
+  // effect에서 사용하는 state를 추가
+  ]);
+
+  
   if (currentPath === "/") {
     dispatch(changePage('대쉬보드 홈'));
   } else if (currentPath === "/Stock") {
