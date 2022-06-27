3bc67e2ad9d8043dad2094232c46ee831d3708d9
gyubin <gbcompany27@gmail.com>
2022-06-27 21:15:44 +0900

    update size policy of line graph

--- src/components/library/ui/unit/LineGraph/LineGraph.js
+++ src/components/library/ui/unit/LineGraph/LineGraph.js
@@ -8,8 +8,8 @@ export default function LineGraph(props) {
     const maximumData = Math.max(...props.ydata);
     const minimumData = Math.min(...props.ydata);
     
-    const plotMaximumValue = maximumData + (maximumData - minimumData) * 0.25;
-    const plotMinimumValue = minimumData - + (maximumData - minimumData) * 0.25;
+    const plotMaximumValue = maximumData + (maximumData - minimumData) * 0.05;
+    const plotMinimumValue = minimumData - (maximumData - minimumData) * 0.3;
 
     return (
         <div className='line-graph-container'>
