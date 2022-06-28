890ba21bf574a676d13851ff2083176a7869c8a6
gyubin <gbcompany27@gmail.com>
2022-06-28 21:33:08 +0900

    add month increase and decrease function in calendar

--- src/components/library/ui/unit/Calendar/Calendar.js
+++ src/components/library/ui/unit/Calendar/Calendar.js
@@ -1,6 +1,25 @@
 import './Calendar.css';
+import React, { useState } from 'react';
 
 export default function Calendar(props) {
+    let today = new Date();
+
+    const [month, setMonth] = useState(today.getMonth());
+    const increaseMonth = () => {
+        if (month === 11) {
+            setMonth(0);
+        } else {
+            setMonth(month + 1);
+        }
+    }
+    const decreaseMonth = () => {
+        if (month === 0) {
+            setMonth(11);
+        } else {
+            setMonth(month - 1);
+        }
+    }
+
     const CalndarDayList = props.dayList.map((day,index) => (
         <div className='calendar-day' key={index}>
             <button className='calendar-day-button'>
@@ -12,14 +31,14 @@ export default function Calendar(props) {
     return (
         <div>
             <div className="calendar-month-row-container">
-                <button className='calendar-month-change-button'>
+                <button className='calendar-month-change-button' onClick={decreaseMonth}>
                     <img className='calendar-month-change-button-image'
                          src={require('../../images/Icon/icon_left_arrow_white.png')}></img>
                 </button>
                 <div style={{width:'30px'}}></div>
-                <h3 className='calendar-month-text'>6월</h3>
+                <h3 className='calendar-month-text'>{month+1}월</h3>
                 <div style={{width:'30px'}}></div>
-                <button className='calendar-month-change-button'>
+                <button className='calendar-month-change-button' onClick={increaseMonth}>
                     <img className='calendar-month-change-button-image'
                          src={require('../../images/Icon/icon_right_arrow_white.png')}></img>
                 </button>
