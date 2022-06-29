efacfe133d8e703867246b326a3b1d93308b8d3f
gyubin <gbcompany27@gmail.com>
2022-06-29 23:45:23 +0900

    adjust news

--- src/components/library/ui/stock/StockNews/StockNews.js
+++ src/components/library/ui/stock/StockNews/StockNews.js
@@ -18,11 +18,6 @@ export default function StockNews() {
                 title='애플 MR 헤드셋에 M2 칩 탑재되나'
                 journal='조선일보'
                 date='2022.06.07'></News>
-            <News
-                image={require('../../images/image/image_product_example.png')}
-                title='애플 MR 헤드셋에 M2 칩 탑재되나'
-                journal='조선일보'
-                date='2022.06.07'></News>
         </div>
     )
 }
