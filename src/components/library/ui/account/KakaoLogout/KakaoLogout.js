ceb58e50023073219647e4aeaee91556ab8a6c75
gyubin <gbcompany27@gmail.com>
2022-07-16 16:08:08 +0900

    update logout function'

--- src/components/library/ui/account/KakaoLogout/KakaoLogout.js
+++ src/components/library/ui/account/KakaoLogout/KakaoLogout.js
@@ -9,15 +9,16 @@ export default function KakaoLogout() {
     const dispatch = useDispatch();
     const KakaoLogoutHandler = () => {
         if (window.Kakao.Auth.getAccessToken()) {
-            window.Kakao.API.request({
-                url: '/v1/user/unlink',
-                success: function (response) {
-                console.log(response);
-                },
-                fail: function (error) {
-                console.log(error);
-                },
-            });
+            window.Kakao.Auth.logout();
+            // window.Kakao.API.request({
+            //     url: '/v1/user/unlink',
+            //     success: function (response) {
+            //     console.log(response);
+            //     },
+            //     fail: function (error) {
+            //     console.log(error);
+            //     },
+            // });
             alert('로그아웃이 완료되었습니다.');
             window.Kakao.Auth.setAccessToken(undefined);
             dispatch(switchLoginStatusFalse());
